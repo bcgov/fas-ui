5699f273f18c975ed30ac3cfab28131af84043c8
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2021-07-23 10:13:56 -0700

    UI changes (#34)

--- src/components/ViewRoutingSlip/RoutingSlipTransaction.vue
+++ src/components/ViewRoutingSlip/RoutingSlipTransaction.vue
@@ -1,23 +1,25 @@
 <template>
-  <div >
-    <h3 data-test="title">03.Routing Slip Transaction</h3>
-    <div class="my-3">
-      <p class="ma-0 align-self-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
-        porttitor sem.
-      </p>
-      <v-btn
-      large
-      color="primary"
-      data-test="btn-add-fund"
-      class="mt-2"
-      @click="close"
-      v-can:fas_edit.hide
-      >
-        <v-icon class="mr-1">mdi-plus</v-icon>
-        <span class="font">Add Transaction Manually</span>
-      </v-btn>
-    </div>
+  <div>
+    <header class="d-flex flex-column mb-0">
+      <div>
+        <div>
+          <h3 data-test="title">03.Routing Slip Transaction</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
+            porttitor sem.
+          </p>
+        </div>
+        <v-btn
+        large
+        color="primary"
+        data-test="btn-add-transaction"
+        v-can:fas_edit.hide
+        >
+          <v-icon class="mr-1">mdi-plus</v-icon>
+          <span class="font">Add Transaction Manually</span>
+        </v-btn>
+      </div>
+    </header>
     <div class="d-flex flex-column">
       <transaction-data-table />
     </div>
@@ -26,19 +28,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import TransactionDataTable from './TransactionDataTable.vue'
-import { useRoutingSlipTransaction } from '@/composables/ViewRoutingSlip'
 
 @Component({
   components: {
     TransactionDataTable
-  },
-  setup (_, context) {
-    const {
-      close
-    } = useRoutingSlipTransaction(_, context)
-    return {
-      close
-    }
   }
 })
 export default class RoutingSlipTransaction extends Vue {}
--- src/components/ViewRoutingSlip/TransactionDataTable.vue
+++ src/components/ViewRoutingSlip/TransactionDataTable.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="header-bg-color d-flex align-center py-5 mb-0">
+    <div class="header-bg-color d-flex align-center py-5 mb-0 mt-3">
       <v-icon color="primary" class="ml-8">
           mdi-view-list
       </v-icon>
-      <p class="ml-2 mb-0 font-weight-bold" data-test="title">Transactions</p><p class="mb-0" v-if="invoiceCount">{{ `(${invoiceCount})` }}</p>
+      <p class="ml-2 mb-0 font-weight-bold" data-test="title">Transactions</p>&nbsp;<p class="mb-0" v-if="invoiceCount">{{ `(${invoiceCount})` }}</p>
     </div>
     <v-data-table
     :headers="headerTranscations"
