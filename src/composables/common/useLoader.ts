f938bd566cd7b235b894f418fb4d1451f5c99026
karthik-aot <karthik.r@aot-technologies.com>
2021-07-30 10:33:36 -0700

    local state

--- src/composables/common/useLoader.ts
+++ src/composables/common/useLoader.ts
@@ -1,17 +1,28 @@
 /*
 Composable function for loader component that is displayed whenever axios async process takes place
+Global loader is used in App.vue where show a full route loader - <loader-component v-show="globalLoader"></loader-component>
+While, isLoading can be used inside an individual component - <loader-component v-if="isLoading"></loader-component>
+<load>
 */
 
 import { createNamespacedHelpers } from 'vuex-composition-helpers'
+import { ref } from '@vue/composition-api'
 
 const loadingStatusModule = createNamespacedHelpers('loadingStatus') // specific module name
 const { useState } = loadingStatusModule
 
 export function useLoader () {
+  const isLoading = ref<boolean>(false)
   // vuex state and mutations
   const { globalLoader } = useState(['globalLoader'])
 
+  function toggleLoading (): void {
+    isLoading.value = !isLoading.value
+  }
+
   return {
-    globalLoader
+    globalLoader,
+    isLoading,
+    toggleLoading
   }
 }
