419aaecda4706b63c989230d4314d650e76b5752
karthik-aot <karthik.r@aot-technologies.com>
2021-08-03 13:50:06 -0700

    refactor loader component

--- src/components/common/LoaderComponent.vue
+++ src/components/common/LoaderComponent.vue
@@ -2,16 +2,19 @@
   <v-container>
     <v-fade-transition>
       <div class="loading-container" data-test="div-loading-container">
-        <v-progress-circular size="50" width="5" color="primary" indeterminate/>
+        <v-progress-circular :size="size" :width="width" :color="color" indeterminate/>
       </div>
     </v-fade-transition>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class LoaderComponent extends Vue {
+  @Prop({ default: 50 }) size: number
+  @Prop({ default: 5 }) width: number
+  @Prop({ default: 'primary' }) color: string
 }
 </script>
