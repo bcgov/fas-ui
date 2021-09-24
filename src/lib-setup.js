d89d267981e30f624c59aad32e4b3d62a5020acf
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-09-24 13:35:30 -0700

    FAS Search - Convert it to common component#8510

--- src/lib-setup.js
+++ src/lib-setup.js
@@ -1,5 +1,6 @@
-
-/* Library build set up for fas-search-components.
+/*
+While upgrading to vue 3 please check full plugin and do necessary modifications here
+  Library build set up for fas-search-components.
   build library using vue-cli-service
   Basic set up of included files store and i18n included here
   How to use :
@@ -7,8 +8,8 @@
   Vue.use(Search, { store, i18n })
   include style if needed either
   import 'fas-ui/fas-lib/fas-ui.css'
-  or inside component style tag us,
-  @import '~fas-ui/fas-lib/fas-ui.css';
+  or inside component style tag us without scope (global scope needed),
+  @import '~fas-ui/src/assets/scss/search.scss';
   */
 import OurVue from 'vue'
 import Search from '@/components/Dashboard/Search.vue'
@@ -23,8 +24,8 @@ import i18n from '@/plugins/i18n'
 /**
  * install function
  *
- * @param {*} Vue parent component vue instance
- * @param {*} optionsneed store and i18n
+ * @param {*} Vue parent component vue contructor
+ * @param {store, i18n} options need store and i18n
  */
 function install (Vue, options) {
   if (OurVue !== Vue) {
@@ -34,9 +35,20 @@ function install (Vue, options) {
 
   if (install.installed) return
   install.installed = true
-  // TODO check before use
-  // if parent is not using VueCompositionAPI api need to push to vue
-  Vue.use(VueCompositionAPI)
+  // since VueCompositionAPI is not giving direct access to property we are checking by using setup api exist in context or not
+  // since setup is a reserved keyword and which is using in vue 3 also, Hope it will not change.
+  // Another solution is Object.hasOwnProperty.call(Vue, '__composition_api_installed__').
+  // we are not using that in futurem, if they change the string no need to brake app
+  // Couldnot find better solution since lack of information available. sorry devs :(
+  if (!Object.hasOwnProperty.call(Vue.config.optionMergeStrategies, 'setup')) {
+    // if parent is not using VueCompositionAPI api need to push to vue
+    Vue.use(VueCompositionAPI)
+    // eslint-disable-next-line no-console
+    console.info('[FAS-Plugin] VueCompositionAPI is not available in parent so pushing from plugin')
+  } else {
+    // eslint-disable-next-line no-console
+    console.info('[FAS-Plugin] VueCompositionAPI already installed')
+  }
 
   // simple hack to inject locale messages. check for better solutions
   // this will not work when chaging lang.
@@ -48,7 +60,7 @@ function install (Vue, options) {
     )
   } else {
     // eslint-disable-next-line no-console
-    console.error('please provide i18n')
+    console.error('[FAS-Plugin] please provide i18n to use lang')
   }
   // pushing store modules to parent store
   // use unique name to avoid conflict
@@ -60,9 +72,9 @@ function install (Vue, options) {
     window.fasStore = options.store
   } else {
     // eslint-disable-next-line no-console
-    console.error('please provide store')
+    console.error('[FAS-Plugin] please provide store')
   }
-  // registering component
+  // registering component to use as  plugin
   Vue.component('fas-search-component', Search)
 }
 
