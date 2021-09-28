871a18109d2662257b521bb3495c131ce24c144b
karthik-aot <karthik.r@aot-technologies.com>
2021-09-28 13:56:10 -0700

    addign comments

--- src/composables/ViewRoutingSlip/useLinkedRoutingSlipDetails.ts
+++ src/composables/ViewRoutingSlip/useLinkedRoutingSlipDetails.ts
@@ -4,6 +4,11 @@ import { toRefs } from '@vue/composition-api'
 export default function useLinkedRoutingSlipDetails (props, context) {
   const { parentRoutingSlipNumber, routingSlipNumber } = toRefs(props)
 
+  /* During these scenarios this component would be displayed:
+    1. Navigate to child routing slip - at which we append the parent id and check if it is a libray call.
+      If so, append queryparam to persist breadcrumbs
+    2. Navigate to parent routing slip - at which we redirect to view routing slip and append queryparams if it is a library call.
+  */
   function navigateTo (): string {
     const redirectFromAuth = context.root.$route?.query?.redirectFromAuth
     if (parentRoutingSlipNumber.value) {
