3683c0375301ade7cf0f254bdde5f7f891507fe1
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2021-10-28 10:06:30 -0700

    Feature/9380 ux assurance fix(#110)

--- src/components/ViewRoutingSlip/TransactionDataTable.vue
+++ src/components/ViewRoutingSlip/TransactionDataTable.vue
@@ -36,7 +36,7 @@
       </div>
     </template>
     <template v-slot:[`item.actions`]="{ item, index }">
-      <span v-if="item.statusCode === InvoiceStatus.REFUNDREQUEST" :data-test="getIndexedTag('text-cancel', index)" class="cancel-text-color font-weight-bold"> Cancelled </span>
+      <span v-if="item.statusCode === InvoiceStatus.REFUNDREQUEST" :data-test="getIndexedTag('text-cancel', index)" class="error--text font-weight-bold"> Cancelled </span>
       <template v-else>
         <v-btn
           outlined
@@ -58,8 +58,8 @@
       :text="modalDialogDetails.modalDialogText"
       dialog-class="notify-dialog"
       data-test="dialog-confirm-cancellation"
-      max-width="679"
-      max-height="310"
+      max-width="639"
+      max-height="327"
       :icon="modalDialogDetails.modalDialogIcon"
       :iconColor="modalDialogDetails.modalDialogIconColor"
     >
@@ -124,10 +124,9 @@ export default class TransactionDataTable extends Vue {
   .header-bg-color {
     background-color: $BCgovBlue0;
   }
-  .fas-transactions tbody tr td {
+  .v-data-table {
+    ::v-deep .v-data-table__wrapper>table>tbody>tr>td {
       padding: 20px 15px !important;
-  }
-  .cancel-text-color {
-    color: $BCgovInputError;
+    }
   }
 </style>
