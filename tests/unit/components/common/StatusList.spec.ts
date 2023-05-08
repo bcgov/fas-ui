52463808cc98ad7b8a9bec507b693b37a1ddd62d
bennypc <52959626+bennypc@users.noreply.github.com>
2023-05-08 11:41:20 -0700

    refactored test to check for direct descriptions

--- tests/unit/components/common/StatusList.spec.ts
+++ tests/unit/components/common/StatusList.spec.ts
@@ -1,4 +1,4 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
 import StatusList from '@/components/common/StatusList.vue'
 import Vuetify from 'vuetify'
 
@@ -6,6 +6,7 @@ import { routingSlipStatusListMock } from '../../test-data/mock-code'
 import { useCodes } from '@/composables/useCodes'
 import CodesService from '@/services/codes.service'
 import { useRoutingSlip } from '@/composables/useRoutingSlip'
+import sinon from 'sinon'
 
 const { routingSlipStatusList, getRoutingSlipStatusList } = useCodes()
 
@@ -31,15 +32,28 @@ describe('StatusList.vue', () => {
     expect(wrapper.find('[data-test="select-status"]').exists()).toBeTruthy()
   })
 
-  it('does not render Refund Rejected option', () => {
-    const wrapper = shallowMount(StatusList, {
+  it('removes Refund Rejected option', async () => {
+    // Mock the API call to return the list of routing slip statuses
+    const sandbox = sinon.createSandbox()
+    const get = sandbox.stub(CodesService, 'getCodes')
+    get.returns(new Promise(resolve => resolve({ data: { codes: routingSlipStatusListMock } })))
+
+    // Render the StatusList component
+    const wrapper = mount(StatusList, {
       localVue,
       vuetify,
       propsData: {
         value: 'ACTIVE'
       }
     })
+
+    // Wait for the component to finish rendering
+    await wrapper.vm.$nextTick()
+
+    // taking all of the descriptions
+    const descriptions = wrapper.vm.routingSlipStatusList.map(status => status.description)
     // Check if the "Refund Rejected" option is not in the status list
-    expect(wrapper.text()).not.toContain('Refund Rejected')
+    expect(descriptions).toContain('Active')
+    expect(descriptions).not.toContain('Refund Rejected')
   })
 })
