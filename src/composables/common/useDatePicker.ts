ab45c7e1a22e736feda68919482ed23f4f8c290c
Travis Semple <travis8814@gmail.com>
2024-01-19 08:04:31 -0800

    Handle null case

--- src/composables/common/useDatePicker.ts
+++ src/composables/common/useDatePicker.ts
@@ -4,6 +4,7 @@ export function useDatePicker (props, emits) {
   const { modelValue } = toRefs(props)
   // Wired up to input.
   const showDateField = computed(() => {
+    if (modelValue.value === '') return null
     const dateObject = (modelValue.value instanceof Date)
       ? DateTime.fromJSDate(modelValue.value) : DateTime.fromFormat(modelValue.value, 'yyyy-LL-dd')
     return dateObject.setZone('America/Vancouver').toFormat('yyyy-LL-dd')
@@ -11,6 +12,7 @@ export function useDatePicker (props, emits) {
   // Wired up to datepicker.
   const dateValue = computed({
     get: () => {
+      if (modelValue.value === '') return null
       const dateObject = (modelValue.value instanceof Date)
         ? DateTime.fromJSDate(modelValue.value) : DateTime.fromFormat(modelValue.value, 'yyyy-LL-dd')
       return dateObject.setZone('America/Vancouver').toJSDate()
